88f6bb5153227309a4b825f0f34ea37584f17025
Karthick090420 <152793152+Karthick090420@users.noreply.github.com>
2026-06-04 13:39:32 +0530

    Create generate.py

---
+++ .github/workflows/generate.py
@@ -0,0 +1,29 @@
+import requests
+import sys
+
+REPO_OWNER = "karthick090420"
+REPO_NAME = "cnetral_repo"
+WORKFLOW_FILE = "sample.yml"
+
+GITHUB_TOKEN = "<PAT_OR_GITHUB_APP_TOKEN>"
+
+repository_url = sys.argv[1]
+
+url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/actions/workflows/{WORKFLOW_FILE}/dispatches"
+
+payload = {
+"ref": "main",
+"inputs": {
+"repository_url": repository_url
+}
+}
+
+headers = {
+"Authorization": f"Bearer {GITHUB_TOKEN}",
+"Accept": "application/vnd.github+json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.status_code)
+print(response.text)
